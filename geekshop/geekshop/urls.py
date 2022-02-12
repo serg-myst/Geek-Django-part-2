f39ed2dddaee9841edd3381143148c37a8a89fc9
Serg <serg_myst@mail.ru>
2022-02-12 18:55:45 +0300

    Lesson 6

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -20,6 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.i18n import set_language
 
+import debug_toolbar
+
 from mainapp.views import index, products
 
 urlpatterns = [
@@ -41,3 +43,4 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [path('debug', include(debug_toolbar.urls))]
