fa4db5de8aec910aeb7dabd0f33bbe63db25eb8d
Serg <serg_myst@mail.ru>
2022-02-12 18:58:14 +0300

    Lesson 6

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -43,4 +43,4 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += [path('debug', include(debug_toolbar.urls))]
+urlpatterns += [path('debug/', include(debug_toolbar.urls))]
